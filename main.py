5efdd6a8345e1399976bb2f1fe2de082eb201091
Martin The Coder <acadecoder@gmail.com>
2021-07-13 15:19:32 -0500

    Game is working

--- main.py
+++ main.py
@@ -2,18 +2,20 @@
 import random
 import time
 
+#Functions
 def showIntro():
-    print('You are in a land full of dragons. In front of you,\nthere are two caves. In one of them, the dragon is kind and\nfriendly and will share its treasure with you. The other dragon\nis greedy and hungry, and will devour you immediately.\n')
+    print('\nYou are in a land full of dragons. In front of you,\nthere are two caves. In one of them, the dragon is kind and\nfriendly and will share its treasure with you. The other dragon\nis greedy and hungry, and will devour you immediately.\n')
 
 def chooseCave():
     cave = ''
     
     while cave != '1' and cave != '2':
-        cave = input('Which cave do you want to enter? (1 or 2)')
+        cave = input('Which cave do you want to enter? (1 or 2)\n')
     
     return cave
 
 def exploreCave(choosenCave):
+    print()
     print('You approach the cave...')
     time.sleep(2)
     print("It's dark and creepy...")
@@ -27,5 +29,18 @@ def exploreCave(choosenCave):
         print('It gives you its treasure!')
     else:
         print('It gobbles you up in one bite!')
+    
+    print()
 
-#------------
+#Functions
+
+playAgain = 'y' #yes
+while playAgain.lower().startswith('y'):
+    
+    showIntro()
+    
+    caveNumber = chooseCave()
+
+    exploreCave(caveNumber)
+
+    playAgain = input("Do you want to play again?\n")
